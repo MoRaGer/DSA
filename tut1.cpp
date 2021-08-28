22d9dda7c77458f9c2864d7f8651cd78d39e7b68
MoRaGer <ncs.thakur@gmail.com>
2021-08-28 15:24:19 +0530

    hello

---
+++ tut1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+// 
+//   int m,n;
+//   cin>>m>>n;
+//   for (int i=1;i<=m;i++){
+    //   for (int j=1;j<=n;j++){
+        //   cout<<"*";
+    //   }
+    //   cout<<endl;
+//   }
+// 
+  int m,n;
+  cin>>m>>n;
+  for (int i=1;i<=m;i++){
+      for (int j=1;j<=n;j++){
+          if (i==1 || i==m){
+              cout<<"*";
+          }
+          else if (j==1 || j==n){
+              cout<<"*";
+          }
+          else {
+              cout<<" ";
+          }
+          
+
+      }cout<<endl;
+  }
+
+    return 0;
+
+}
